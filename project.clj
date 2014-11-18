b9745a5569335fd2007675af7f40ac17b8cb4552
Michael Rubanov <mrubanov@gmail.com>
2014-11-18 23:55:34 +0200

    updated deps

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject clj-todo "0.4.0-SNAPSHOT"
-  :description "A small lib for adding todo annotations to Clojure projects."
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-		 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-		 [leiningen "1.1.0"]]
+(defproject micrub/clj-todo "0.4.1-SNAPSHOT"
+  :description "A small lib for adding todo annotations to Clojure projects.
+                Forked from https://github.com/tgk/clj-todo"
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :dev-dependencies [[autodoc "0.7.1"]]
   :autodoc {:copyright "2010 Thomas G. Kristensen"}
   :todo-log "todo-summary.log")
