f2ae5aeb1b90614c25f91f4f6b7468be931f1604
Michael Rubanov <mrubanov@gmail.com>
2014-11-19 00:14:16 +0200

    @major @format

--- src/leiningen/todo.clj
+++ src/leiningen/todo.clj
@@ -5,23 +5,23 @@
 
 (defn todo
   "Prints a summary of todos annotated using clj-todo.todo/todo.
-If namespaces are given as commandline args, prints the summary
-for those namespaces; otherwise prints the summary for all the
-:namespaces in project.clj. If :todo-log is given in project, 
-also writes the log to the filename in :todo-log."
+  If namespaces are given as commandline args, prints the summary
+  for those namespaces; otherwise prints the summary for all the
+  :namespaces in project.clj. If :todo-log is given in project,
+  also writes the log to the filename in :todo-log."
   [project & namespaces]
   (let [namespaces (if (seq namespaces)
-		     (map symbol namespaces)		     
-		     (find-namespaces-in-dir (file (:source-path project))))]
+                     (map symbol namespaces)
+                     (find-namespaces-in-dir (file (:source-path project))))]
     (delete-file-recursively (file (:compile-path project)) true)
     (eval-in-project project
-		     `(do
-			(require '~'clj-todo)
-			(apply require '~namespaces)
-			(clj-todo/todo-summary)))
-    (if (contains? project :todo-log) 
-      (eval-in-project project 
-		     `(do
-			(require '~'clj-todo)
-			(apply require '~namespaces)
-			(clj-todo/todo-summary-file ~(:todo-log project)))))))
+                     `(do
+                        (require '~'clj-todo)
+                        (apply require '~namespaces)
+                        (clj-todo/todo-summary)))
+    (if (contains? project :todo-log)
+      (eval-in-project project
+                       `(do
+                          (require '~'clj-todo)
+                          (apply require '~namespaces)
+                          (clj-todo/todo-summary-file ~(:todo-log project)))))))
