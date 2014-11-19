ae44d41a913fe7dfcc10b465bf21eb1a529a42c6
Michael Rubanov <mrubanov@gmail.com>
2014-11-19 22:48:35 +0200

    veriosn bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject micrub/clj-todo "0.4.1-SNAPSHOT"
+(defproject micrub/clj-todo "0.5.0"
   :description "A small lib for adding todo annotations to Clojure projects.
                 Forked from https://github.com/tgk/clj-todo"
   :dependencies [[org.clojure/clojure "1.6.0"]
