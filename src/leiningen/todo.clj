dc76b3f612bb1fbaed7f6e5875af8fecca3639fd
Michael Rubanov <mrubanov@gmail.com>
2014-11-19 02:12:20 +0200

    private util functions for finding namespaces based on :source-paths in project.clj , if noting defined default :source-paths is used based on leiningen.core.project.defaults.

--- src/leiningen/todo.clj
+++ src/leiningen/todo.clj
@@ -1,6 +1,7 @@
 (ns leiningen.todo
   (:use [leiningen.core.eval :only (eval-in-project)])
   (:use [clojure.java.io :only [file]])
+  (:require [leiningen.core.project :as p])
   (:use [clojure.tools.namespace.find :only [find-namespaces-in-dir]]))
 
 (defn delete-recursively [fname]
@@ -10,6 +11,27 @@
                    (func func f2)))
                (clojure.java.io/delete-file f))]
     (func func (clojure.java.io/file fname))))
+
+(defn- ns-find-waring [e dir-path]
+  (format
+    "value: %s in source paths collection caused %s"
+    dir-path
+    (style (.toString e) :red)))
+
+(defn- get-source-paths [project]
+  (let [source-paths-key :source-paths]
+    (concat (source-paths-key p/defaults)
+            (source-paths-key (source-paths-key project)))))
+
+(defn- safely-find-namespaces-in-dir [^String dir-path]
+  (try
+    (find-namespaces-in-dir (file dir-path))
+    (catch Exception e (print-warning (ns-find-waring e dir-path)))))
+
+(defn- get-namespaces [source-paths]
+  (map
+    safely-find-namespaces-in-dir
+    source-paths))
 
 (defn todo
   "Prints a summary of todos annotated using clj-todo.todo/todo.
