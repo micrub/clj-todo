9218c8b3872c60bd891f3a4daecc0eb36a82d8bf
Michael Rubanov <mrubanov@gmail.com>
2014-11-20 01:55:51 +0200

    * adding repository url

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject micrub/clj-todo "0.5.0"
   :description "A small lib for adding todo annotations to Clojure projects.
                 Forked from https://github.com/tgk/clj-todo"
+  :url "https://github.com/micrub/clj-todo"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [leiningen-core "2.5.0"]
                  [org.clojure/tools.namespace "0.2.7"]]
