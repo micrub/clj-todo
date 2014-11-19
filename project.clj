ec72fdf8434249aca396d67dbf1305430f086377
Michael Rubanov <mrubanov@gmail.com>
2014-11-19 22:56:47 +0200

    Conflicts: 	project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject micrub/clj-todo "0.4.1-SNAPSHOT"
+(defproject micrub/clj-todo "0.5.0"
   :description "A small lib for adding todo annotations to Clojure projects.
                 Forked from https://github.com/tgk/clj-todo"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [leiningen-core "2.5.0"]
                  [org.clojure/tools.namespace "0.2.7"]]
-  :dev-dependencies [[autodoc "0.7.1"]]
   :autodoc {:copyright "2010 Thomas G. Kristensen"}
   :todo-log "todo-summary.log")
